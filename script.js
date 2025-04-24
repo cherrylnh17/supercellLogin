4f4da367d43ac75ed8d99f8b14c76094e8d1d38d
Cherry <140305455+cherrylnh17@users.noreply.github.com>
2025-04-24 18:34:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 const progressBar = document.getElementById('progressBar');
 const progressIndicator = document.getElementById('progressIndicator');
+const emailInput = document.getElementById('emailInput');
+
+const container1 = document.querySelector('.isi-container1');
+const container2 = document.querySelector('.isi-container2');
 
 function startVerification() {
-  progressBar.style.width = '100%';
-  progressIndicator.style.left = 'calc(100% - 20px)'; // Adjust for indicator size
+    if (emailInput.value.trim() === '') {
+        return;
+    }
+
+    const email = emailInput.value;
+    progressBar.style.width = '100%';
+    progressIndicator.style.left = 'calc(100% - 20px)';
+    document.getElementById('emailTerkirim').textContent = email;
+
+
+    container1.classList.remove('show');
+    container2.classList.add('show');
 }
 
 function kembaliVerification() {
     progressBar.style.width = '0%';
     progressIndicator.style.left = '5px';
+    emailInput.value = '';
+
+    container2.classList.remove('show');
+    container1.classList.add('show');
 }
+
+
